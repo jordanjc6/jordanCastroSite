8764a029f82f1af1b2d5e39b85e102f3e1181d95
Jordan Castro <jordan_castro@dealer-fx.com>
2024-09-24 16:00:01 -0400

    scaffold, create skeleton comps, add header and router to app root

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'JC Site',
+        component: HomeComponent,
+    },
+    {
+        path: 'projects',
+        title: 'JC Projects',
+        component: ProjectsComponent,
+    },
+    {
+        path: 'contact',
+        title: 'JC Contact',
+        component: ContactComponent,
+    },
+];
